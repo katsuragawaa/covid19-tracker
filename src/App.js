418680b9429da8f966c777da664ede1979b3fd77
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-03 12:16:05 -0300

    Fetch api on mount

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-import { Cards, Chart, CountryPicker } from './components/index';
+import { fetchData } from './api';
+
+import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 
 class App extends React.Component {
+  // class based component for easily fetch data on mount
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data)
+  }
+
   render() {
     return (
       <div className={styles.container}>
