7efb2499f61013bf2eeaab1959b6695c274546ff
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-03 17:55:06 -0300

    Handle when global is selected

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -8,17 +8,22 @@ import styles from './CountryPicker.module.css';
 const CountryPicker = ({ handleCountryChange }) => {
   const [fetchedCountries, setFetchedCountries] = useState([]);
 
+  const handleChange = (event) => {
+    let selectedCountry = event.target.value;
+    if (selectedCountry === 'global') selectedCountry = undefined;
+
+    handleCountryChange(selectedCountry);
+  };
+
   useEffect(() => {
     const fetchApi = async () => setFetchedCountries(await fetchCountries());
+
     fetchApi();
   }, [setFetchedCountries]);
 
   return (
     <FormControl className={styles.formControl}>
-      <NativeSelect
-        defaultValue={''}
-        onChange={(e) => handleCountryChange(e.target.value)}
-      >
+      <NativeSelect defaultValue={''} onChange={handleChange}>
         <option value="global">Global</option>
         {fetchedCountries.map((country, index) => (
           <option value={country} key={index}>
