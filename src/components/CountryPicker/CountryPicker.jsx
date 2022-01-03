2e3eb1f38cc5fb5c537b9783d5ce0a5c1a394594
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-03 17:09:01 -0300

    Create countries select

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+
+import { fetchCountries } from '../../api';
+
+import styles from './CountryPicker.module.css';
 
 const CountryPicker = () => {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchApi = async () => setFetchedCountries(await fetchCountries());
+    fetchApi();
+  }, [setFetchedCountries]);
+
   return (
-  <h1>CountryPicker</h1>
+    <FormControl className={styles.formControl}>
+      <NativeSelect>
+        <option value="global">Global</option>
+        {fetchedCountries.map((country, index) => (
+          <option value={country} key={index}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
   );
 };
 
